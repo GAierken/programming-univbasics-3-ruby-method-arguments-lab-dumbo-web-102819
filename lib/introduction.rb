84824a563c3ea295e3274cdb3816693aab2539c0
Guligena Aierken <guligena_aierken@163.com>
2019-10-10 02:24:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,10 @@ end
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-def introduction_with_language_optional
+def introduction_with_language_optional(name, language_1="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language_1}."
+end
+
 
 
 
